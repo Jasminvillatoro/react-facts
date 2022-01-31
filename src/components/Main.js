57dc64e06f2c5fd6a368f093a4721baa79f8c9b3
jasmin villatoro <jasminvilla321@gmail.com>
2022-01-31 14:49:53 -0500

    main setup

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,15 @@
 const Main = () => {
   return (
-    <div>
-      <h1>main</h1>
-    </div>
+    <main>
+      <h1 className="react-facts">Fun facts about React</h1>
+      <ul className="list-cnt">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
   );
 };
 
